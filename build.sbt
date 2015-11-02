7c1f5625455510cf1b9330d2c77e840f85b897ae
eftov <b.fovet@gmail.com>
2015-11-02 13:11:56 +0100

    Add source with tweets test

---
+++ build.sbt
@@ -0,0 +1,25 @@
+lazy val root = (project in file(".")).
+  settings(
+    name := "spark-streaming-test",
+    version := "1.0",
+    scalaVersion := "2.11.7",
+    mainClass in Compile := Some("Streamer")
+  )
+
+libraryDependencies ++= Seq(
+  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3",
+  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3",
+  "org.apache.spark" %% "spark-core" % "1.5.1" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "1.5.1" % "provided",
+  "org.apache.spark" %% "spark-streaming-twitter" % "1.5.1",
+  "org.twitter4j" % "twitter4j-core" % "3.0.3",
+  "org.twitter4j" % "twitter4j-stream" % "3.0.3"
+)
+
+// META-INF discarding
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+   {
+    case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+    case x => MergeStrategy.first
+   }
+}
---
+++ src/main/scala/Streamer.scala
@@ -0,0 +1,46 @@
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
+
+import scala.io.Source._
+
+object Streamer {
+  def configureTwitterCredentials() = {
+    val file = getClass().getResourceAsStream("twitter_credentials.txt")
+    for (line <- fromInputStream(file).getLines()) {
+      val key :: value :: _ = line.replace(" ","").split("=").toList
+      val fullKey = "twitter4j.oauth." + key;
+      System.setProperty(fullKey, value)
+    }
+  }
+
+  def main(args: Array[String]) {
+
+    configureTwitterCredentials()
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("WordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+
+    val filters = Seq("orange_france","france")
+
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+
+    val statuses = stream.map(status => status.getText())
+    statuses.print()
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map{case (topic, count) => (count, topic)}
+      .transform(_.sortByKey(false))
+
+    // Print popular hashtags
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(5)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
