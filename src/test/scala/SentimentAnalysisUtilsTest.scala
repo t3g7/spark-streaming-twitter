1fae677779299c36f0646cbf21db91880489ce9c
eftov <b.fovet@gmail.com>
2015-12-07 09:35:52 +0100

    Test real tweets

--- src/test/scala/SentimentAnalysisUtilsTest.scala
+++ src/test/scala/SentimentAnalysisUtilsTest.scala
@@ -9,28 +9,49 @@ class SentimentAnalysisUtilsTest extends FunSpec with ShouldMatchers{
     }
 
     it ("should detect a negative statement") {
-      detectSentiment("I am feeling very sad and frustrated.") should equal (NEGATIVE)
+      detectSentiment("Wifi down @virginmedia  & thanks to @o2 no back up with a phone signal in QUORN - #1stworldproblems I know.") should equal (NEGATIVE)
+    }
+
+    it ("should detect a negative statement") {
+      detectSentiment("Thanks @O2 phone merrily allowed to go beyond data allowance. No capping! Extortionate bill at Xmas brilliant service.") should equal (NEGATIVE)
+    }
+
+    it ("should detect a negative statement") {
+      detectSentiment("@O2 just 'upgraded'. Shame my upgrade doesn't work properly! Absolutely livid!") should equal (NEGATIVE)
     }
 
     it ("should detect a neutral statement") {
-      detectSentiment("I'm watching a movie") should equal (NEUTRAL)
+      detectSentiment("@O2 iPhone 6s") should equal (NEUTRAL)
+    }
+
+    it ("should detect a neutral statement") {
+      detectSentiment("@O2 has it been removed?") should equal (NEUTRAL)
     }
 
     it ("should detect a positive statement") {
-      detectSentiment("C'était une bonne expérience.") should equal (POSITIVE)
+      detectSentiment("@O2 Great giveaway #O2CyberMonday") should equal (POSITIVE)
+    }
+
+    it ("should detect a positive statement") {
+      detectSentiment("@O2 wow what Amazing Bundle Would make my Christmas #O2CyberMonday") should equal (POSITIVE)
+    }
+
+    it ("should detect a positive statement") {
+      detectSentiment("@O2 Looks fantastic - very generous!  #O2CyberMonday") should equal (POSITIVE)
+    }
+
+    it ("should detect a positive statement") {
+      detectSentiment("@o2 just made my day, little stars they are") should equal (POSITIVE)
     }
 
     it ("should detect a very positive statement") {
-      detectSentiment("It was a very nice experience.") should equal (VERY_POSITIVE)
+      detectSentiment("@O2 Wow \uD83D\uDE0D fantastic prize, u guys are so so kind, got my fingers tightly crossed \uD83D\uDE00") should equal (VERY_POSITIVE)
     }
 
-    it("should detect a negative sentiment on a full review") {
-      detectSentiment(
-        """
-          |This movie doesn't care about cleverness, wit or any other kind of intelligent humor.
-          |Those who find ugly meanings in beautiful things are corrupt without being charming.
-          |There are slow and repetitive parts, but it has just enough spice to keep it interesting.
-        """.stripMargin) should equal (NEGATIVE)
+    it ("should detect a very positive statement") {
+      detectSentiment("@O2  Wow what a fab prize!!! \uD83C\uDF81 #O2CyberMonday") should equal (VERY_POSITIVE)
     }
+
+
   }
 }
